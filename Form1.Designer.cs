9f7c08cc96ab97a444c8144054743e5deeca6595
PsykoDev <johann.l_62@hotmail.fr>
2021-07-16 22:06:48 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace Ping {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "192.168.1.1",
+            "google.com",
+            "8.8.8.8",
+            "1.1.1.1"});
+            this.comboBox1.Location = new System.Drawing.Point(14, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "IP";
+            this.textBox1.Size = new System.Drawing.Size(121, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(141, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(340, 446);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(487, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Min : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(487, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Max : ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(14, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PlaceholderText = "SPEED MS";
+            this.textBox2.Size = new System.Drawing.Size(123, 23);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "1500";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(14, 186);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 19);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Loop ping";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(14, 435);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(525, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 15);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "999";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(525, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(487, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Moy : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(525, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(487, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Total : ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(525, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 470);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ping {
+    public partial class Form1 : Form {
+        private int count;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            comboBox1.SelectedIndex = 0;
+            if((File.Exists(@"ping.txt") && new FileInfo(@"ping.txt").Length > 0)) {
+                StreamReader sr = new StreamReader(@"ping.txt");
+                string item = sr.ReadLine();
+                int index = comboBox1.FindString(item);
+                if((index != -1 & !string.IsNullOrEmpty(item))) {
+                    comboBox1.SelectedIndex = index;
+                }
+                sr.Close();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            StreamWriter wr = new StreamWriter(@"ping.txt");
+            wr.WriteLine(comboBox1.Text);
+            wr.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if(checkBox1.Checked) {
+                timer1.Start();
+                return;
+            }
+            System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 10000;
+            PingReply r;
+            string s;
+            s = textBox1.Text;
+            if(s == "")
+                s = comboBox1.GetItemText(comboBox1.SelectedItem);
+            r = p.Send(s, timeout, buffer);
+            if(r.Status == IPStatus.Success) {
+                richTextBox1.AppendText(Environment.NewLine + " Successful" + " Response delay = " + r.RoundtripTime.ToString() + " ms");
+                count++;
+            } else if(r.Status == IPStatus.TimedOut) {
+                richTextBox1.AppendText(Environment.NewLine + "TIMED OUT");
+            }
+            if(r.RoundtripTime > int.Parse(label6.Text)) {
+                label6.Text = r.RoundtripTime.ToString();
+            }
+            if(r.RoundtripTime < int.Parse(label5.Text)) {
+                label5.Text = r.RoundtripTime.ToString();
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e) {
+            if(textBox1.Text == string.Empty) {
+                richTextBox1.Text = "Nothing Added";
+            } else {
+                comboBox1.Items.Add(textBox1.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            timer1.Interval = int.Parse(textBox2.Text);
+            System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 10000;
+            PingReply r;
+            string s;
+            s = textBox1.Text;
+            if(s == "")
+                s = comboBox1.GetItemText(comboBox1.SelectedItem);
+            r = p.Send(s, timeout, buffer);
+            if(r.Status == IPStatus.Success) {
+                richTextBox1.AppendText(Environment.NewLine + " Successful" + " Response delay = " + r.RoundtripTime.ToString() + " ms");
+                count++;
+            } else if(r.Status == IPStatus.TimedOut) {
+                richTextBox1.AppendText(Environment.NewLine + "TIMED OUT");
+            }
+            if(r.RoundtripTime > int.Parse(label6.Text)) {
+                label6.Text = r.RoundtripTime.ToString();
+            }
+            if(r.RoundtripTime < int.Parse(label5.Text)) {
+                label5.Text = r.RoundtripTime.ToString();
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            richTextBox1.Text = string.Empty;
+            label6.Text = "0";
+            label5.Text = "999";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+        }
+    }
+}
